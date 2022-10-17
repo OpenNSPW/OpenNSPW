07947e48e61a3fc93c8d7b29feeda85c7f321334
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-10-17 11:45:40 +0000

    Fix tests

--- OpenNspw.Tests/FakeAssetManager.cs
+++ OpenNspw.Tests/FakeAssetManager.cs
@@ -5,6 +5,13 @@ namespace OpenNspw.Tests;
 
 internal sealed class FakeAssetManager : IAssetManager
 {
-	public IReadOnlyDictionary<string, Texture2D> Textures { get; } = UnitOptions.Components.Keys.ToDictionary(k => $"Textures/Units/{k}", k => (Texture2D)null!);
+	public IReadOnlyDictionary<string, Texture2D> Textures { get; } = UnitOptions.Components.Keys
+		.Concat(new[]
+		{
+			"ft_usa_folded",
+			"at_usa_folded",
+		})
+		.ToDictionary(k => $"Textures/Units/{k}", k => (Texture2D)null!);
+
 	public IReadOnlyDictionary<string, SoundEffect> SoundEffects { get; } = new Dictionary<string, SoundEffect>();
 }
