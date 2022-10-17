25091354b271388f2aa6b7f30d099af49a188a20
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-10-17 11:56:58 +0000

    Use record structs

--- OpenNspw/CVec.cs
+++ OpenNspw/CVec.cs
@@ -1,21 +1,8 @@
 namespace OpenNspw;
 
-internal readonly struct CVec : IEquatable<CVec>
+internal readonly record struct CVec(int X, int Y)
 {
 	public static readonly CVec Zero = new();
-
-	public int X { get; }
-	public int Y { get; }
-
-	public CVec(int x, int y) => (X, Y) = (x, y);
-
-	public static bool operator ==(CVec left, CVec right) => left.Equals(right);
-	public static bool operator !=(CVec left, CVec right) => !left.Equals(right);
-
-	public bool Equals(CVec other) => X == other.X && Y == other.Y;
-	public override bool Equals(object? obj) => obj is CVec other && Equals(other);
-
-	public override int GetHashCode() => (X, Y).GetHashCode();
 
 	public override string ToString() => (X, Y).ToString();
 }
--- OpenNspw/WRect.cs
+++ OpenNspw/WRect.cs
@@ -1,12 +1,7 @@
 namespace OpenNspw;
 
-internal readonly struct WRect : IEquatable<WRect>
+internal readonly record struct WRect(WPos Center, WVec Size)
 {
-	public WPos Center { get; }
-	public WVec Size { get; }
-
-	private WRect(WPos center, WVec size) => (Center, Size) = (center, size);
-
 	public float Left => Center.X - Size.X / 2;
 	public float Bottom => Center.Y - Size.Y / 2;
 	public float Right => Center.X + Size.X / 2;
@@ -17,15 +12,7 @@ internal readonly struct WRect : IEquatable<WRect>
 	public WPos TopRight => new(Right, Top);
 	public WPos TopLeft => new(Left, Top);
 
-	public static bool operator ==(WRect left, WRect right) => left.Equals(right);
-	public static bool operator !=(WRect left, WRect right) => !left.Equals(right);
-
 	public static WRect FromCenter(WPos center, WVec size) => new(center, size);
-
-	public bool Equals(WRect other) => Center == other.Center && Size == other.Size;
-	public override bool Equals(object? obj) => obj is WRect other && Equals(other);
-
-	public override int GetHashCode() => (Center, Size).GetHashCode();
 
 	public override string ToString() => $"(X: {Left}, Y: {Bottom}, Width: {Size.X}, Height: {Size.Y})";
 
